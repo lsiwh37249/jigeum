d5689eaccdd97f8c49a5153c9bcdffa07ed3fb4b
lsiwh37249 <lsiwh37249@gmail.com>
2024-09-20 14:33:11 +0900

    jigeum_seoul_time()

--- src/jigeum/main.py
+++ src/jigeum/main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import pytz                                                                                                                                                                                 
-def jigeum():
+def jigeum_seoul_time():
     time = datetime.now(pytz.timezone('Asia/Seoul'))
     formatted_time = time.strftime("%Y-%m-%d %H:%M:%S")
     
